d27ea7f4bc67496ca12a79e01ba6a67bb3587dd4
enek0s <enekoalabort@gmail.com>
2026-09-17 12:39:56 +0200

    Hautazkoa

---
+++ generaCSV_bizikletak.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+SARRERA_BIZIKLETA = "datu_iturriak/bizikletak.csv"
+SARRERA_ERABILERAK = "datu_iturriak/erabilerak.csv"
+
+MOTA = "Elektrikoa"
+
+def main():
+      
+   # Extract 
+   df_bizikleta = pd.read_csv(SARRERA_BIZIKLETA)
+   df_erabilera = pd.read_csv(SARRERA_ERABILERAK)
+
+   df_bizikleta_mota = df_bizikleta[df_bizikleta["Mota"] == MOTA]
+
+   print(df_bizikleta_mota.to_string)
